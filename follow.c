254e1265b7a69d54121787233456136061b115dc
Blank <58242347+TheBlankness@users.noreply.github.com>
2023-05-27 10:46:48 +0800

    Update follow.c

--- follow.c
+++ follow.c
@@ -19,3 +19,36 @@ void FollowLine(void *data)
         OSTimeDlyHMSM(0, 0, 0, 100);  // Task period ~ 100 ms
     }
 }
+
+
+void Navig(void *data)
+{
+    for (;;)
+    {
+        if (myrobot.obstacle == 1)  // If blocked, reverse at a slower speed
+        {
+            myrobot.rspeed = -LOW_SPEED;
+            myrobot.lspeed = -LOW_SPEED;
+        }
+        else  // If no obstacle, move forward at a higher speed
+        {
+            myrobot.rspeed = MEDIUM_SPEED;
+            myrobot.lspeed = MEDIUM_SPEED;
+        }
+
+        if (robo_lightSensor() > 60)  // If it's too bright, turn right at a slower speed
+        {
+            myrobot.rspeed = -LOW_SPEED;
+            myrobot.lspeed = LOW_SPEED;
+        }
+
+        OSTimeDlyHMSM(0, 0, 0, 500);  // Task period ~ 500 ms
+    }
+}
+
+void FollowLine(void *data);
+
+OSTaskCreate(FollowLine,                              /* Task function                 */
+            (void *)0,                                /* nothing passed to task        */
+            (void *)&FollowLineStk[TASK_STK_SZ - 1],  /* stack allocated to task       */
+            TASK_FOLLOWLINE_PRIO);                     /* priority of task              */
